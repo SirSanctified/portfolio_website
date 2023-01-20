3158865e5cd9f812a9d0670d152e64cde2a42da9
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-01-20 15:50:51 +0200

    Update requirements.txt

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': os.getenv('DATABASE_NAME'),
         'USER': os.getenv("DATABASE_USER"),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
+        'HOST': os.getenv('DATABASE_URL'),
         'PORT': '',
     }
 }
