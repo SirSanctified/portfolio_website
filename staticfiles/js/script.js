7a8baf440ab066ffb6bb6f0b062bcfca1ce9099c
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-02-05 10:36:15 +0200

    Collectstatic

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,8 +1,13 @@
 const btn = document.getElementById('menu-btn')
-const nav = document.getElementsByClassName('navbar')
+const nav = document.querySelector('.navbar')
+const navbar = document.querySelector('nav')
 
 btn.addEventListener('click', () => {
-    // btn.classList.toggle('open')
-    nav.style.display = "flex"
-    nav.style.transform = "translateX(0)"
+    btn.classList.toggle('open')
+    if (nav.style.display === 'flex') {
+        nav.style.display = 'none'
+    } else {
+        nav.style.display = 'flex'
+        nav.style.transform = 'translateX(0)'
+    }
 })
